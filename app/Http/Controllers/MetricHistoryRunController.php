41c6b00e17beb3bc953f491230ae5f98581d7acd
Mateo Ghidini <mateghidini123@gmail.com>
2024-01-24 14:41:38 -0300

    Create models controlles and migrations

---
+++ app/Http/Controllers/MetricHistoryRunController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\MetricHistoryRun;
+use App\Http\Requests\StoreMetricHistoryRunRequest;
+use App\Http\Requests\UpdateMetricHistoryRunRequest;
+
+class MetricHistoryRunController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreMetricHistoryRunRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(MetricHistoryRun $metricHistoryRun)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(MetricHistoryRun $metricHistoryRun)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateMetricHistoryRunRequest $request, MetricHistoryRun $metricHistoryRun)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(MetricHistoryRun $metricHistoryRun)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/StrategyController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Strategy;
+use App\Http\Requests\StoreStrategyRequest;
+use App\Http\Requests\UpdateStrategyRequest;
+
+class StrategyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreStrategyRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Strategy $strategy)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Strategy $strategy)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateStrategyRequest $request, Strategy $strategy)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Strategy $strategy)
+    {
+        //
+    }
+}
