a417a5e04b2aaf3dfd607d7a74023ac0b5c07226
Mateo Ghidini <mateghidini123@gmail.com>
2024-01-24 15:12:25 -0300

    Add relationship between MetricHistoryRun and Strategy

--- app/Models/MetricHistoryRun.php
+++ app/Models/MetricHistoryRun.php
@@ -5,7 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+
 class MetricHistoryRun extends Model
 {
     use HasFactory;
+
+    public function strategy()
+    {
+        return $this->belongsTo(Strategy::class);
+    }
 }
--- app/Models/Strategy.php
+++ app/Models/Strategy.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Strategy extends Model
 {
     use HasFactory;
+
+    public function metricHistoryRun()
+    {
+        return $this->belongsTo(MetricHistoryRun::class);
+    }
 }
